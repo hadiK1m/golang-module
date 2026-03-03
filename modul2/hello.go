66b3238e9fd9d9b942ba0cecea11760bdd31f502
unknown <haadinurhakim@gmail.com>
2026-03-03 18:42:46 +0700

    add control.go with if-else, for loop, and switch case examples

--- modul2/hello.go
+++ modul2/hello.go
@@ -1,5 +1,6 @@
 package main
 
 func main() {
+	// Menampilkan pesan "Hello, World!"
 	println("Hello, World!")
 }
--- modul3/variable.go
+++ modul3/variable.go
@@ -3,16 +3,21 @@ package main
 import "fmt"
 
 func main() {
+
+	// Variabel
 	var nama string = "Hadi"
 	umur := 25
 	tinggi := 167.5
 	isDeveloper := true
 
+
+	// Menampilkan nilai variabel
 	fmt.Println("Nama:", nama)
 	fmt.Println("Umur:", umur)
 	fmt.Println("Tinggi:", tinggi)
 	fmt.Println("Developer:", isDeveloper)
 
+	// Operasi aritmatika
 	total := umur + 5
 	fmt.Println("5 tahun lagi umur saya adalah:", total)
 }
